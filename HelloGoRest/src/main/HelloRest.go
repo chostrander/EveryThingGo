15e479e0878acfc178aa93e7e83cf417b9ae5539
Carl Hostrander <chostrander@gmail.com>
2018-07-25 21:58:37 -0500

    Update HelloRest.go

--- HelloGoRest/src/main/HelloRest.go
+++ HelloGoRest/src/main/HelloRest.go
@@ -5,13 +5,28 @@ import (
 	"net/http"
 	"github.com/gorilla/mux"
 	"fmt"
+	"encoding/json"
 )
 
 func main() {
+	finish := make(chan bool)
 	router := mux.NewRouter()
 	router.HandleFunc("/hello/{name}",HelloWorld).Methods("GET")
-	log.Fatal(http.ListenAndServe(":8080",router))
+	
+	go func() {
+	    log.Fatal(http.ListenAndServe(":8080",router))
+	}
+	
+	router2 := mux.NewRouter()
+	router2.HandleFunc("/health", GetHealth).Methods("GET")
+	go func() {
+		log.Fatal(http.ListenAndServe(":9000", router2))
+	}
+	<- finish
+}
 
+func GetHealth(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
 }
 
 func HelloWorld (w http.ResponseWriter, r *http.Request) {
